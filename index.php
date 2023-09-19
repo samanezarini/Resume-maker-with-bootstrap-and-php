14f26cfe6db5989fac31d5496b2abebb0c9c5d52
Samane <133226049+samanezarini@users.noreply.github.com>
2023-09-19 15:23:25 +0330

    index
    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+﻿<?php
+require_once 'includes/dbconfig.php';
+require_once 'includes/header.php';
+?>
+    <!-- start-->
+    <header>
+        <div class="cover" style="background-color: #f4f6f8;">
+            <div class="container px-2">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mt-5 pt-lg-5">
+                            <h2>رزومه حرفه ای با رزومه ساز سما</h2>
+                            <p class="lead mt-4">
+                                یک رزومه شغلی به صورت رایگان بسازید،
+                                و با آن خود را متمایز کنید.
+                                دارای قابلیت هایی ویژه، نکات تخصصی، قالب های قابل تنظیم و دانلود سریع PDF  است .
+                            </p>
+                            <div class="mt-3 aos-init aos-animate">
+                                <a class="btn btn-warning shadow-sm mt-1 me-2"href="resume.php">
+                                    شروع کنید
+                                    <i class="fas fa-arrow-right ms-1"></i>
+                                </a>
+                                <a class="btn btn-outline-secondary mt-1" href="video.html" id="btn-movi">
+                                    <i class="fas fa-play-circle me-1"></i> تماشای ویدئو
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 p-3 pe-lg-0 animate">
+                        <img class="pt-2 img-fluid" src="images/building_websites.svg" alt="welcome">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12 col-sm-12 pe-lg-6">
+            <img class="img-fluid" src="images/wave.svg" alt="hello" style="width:1550px;">
+        </div>
+    </header>
+
+
+    <!--Body content-->
+
+        <div class="container">
+            <div class="row py-3 py-lg-5">
+                <div class="col-lg-6 col-md-5 px-3 order-md-last">
+                    <img class="my-3 img-fluid" src="images/development.svg" alt="development" />
+                </div>
+                <div class="col-lg-6 col-md-7 px-2">
+                    <div class="h3 mb-2">رزومه کاری چیست؟</div>
+                    <p class="text-secondary mb-3">
+                        رزومه نوشته ای است که بیشینه سوابق تحصیلی، مهارت های شغلی، تجربیات کاری و مهارت های فرد جویای کار در آن نوشته میشود و به صورت فایل پی دی اف یا چاپ شده ارائه میگردد.
+                        به این دلیل داشتن یک رزومه کاری مناسب و با طراحی اصولی برای فرد متقاضی استخدام بسیار ضروری است. مطمئنا هر چه رزومه کاری شما زیباتر باشد و جزئیات بیشتری از شما ارائه کند، تأثیرگذارتر خواهد بود.
+                    </p>
+                    <p class="text-secondary">از موارد مهم سایت رزومه سما:</p>
+                    <div class="row">
+                        <div class="col-lg-6 mb-2">
+                            <div class="border border-1 border-success rounded px-2 py-2 d-flex align-items-center">
+                                <div class="badge"><img src="images/tick.png" alt="tick" width="30px" height="30px" /></div>
+                                <div class="text-secondary fw-bold">نمونه رزومه های آماده </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 mb-2">
+                            <div class="border border-1 border-success rounded px-2 py-2 d-flex align-items-center">
+                                <div class="badge"><img src="images/tick.png" alt="tick" width="30px" height="30px"/></div>
+                                <div class="text-secondary fw-bold">راهنمای نگارش تخصصی</div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 mb-2">
+                            <div class="border border-1 border-success rounded px-2 py-2 d-flex align-items-center">
+                                <div class="badge"><img src="images/tick.png" alt="tick" width="30px" height="30px" /></div>
+                                <div class="text-secondary fw-bold">نرم افزار قدرتمند سازنده</div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 mb-2">
+                            <div class="border border-1 border-success rounded border-s px-2 py-2 d-flex align-items-center">
+                                <div class="badge"><img src="images/tick.png" alt="tick" width="30px" height="30px" /></div>
+                                <div class="text-secondary fw-bold">نمونه رزومه های موفق</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row py-3 py-lg-5">
+                <div class="col-lg-6 col-md-5 px-3">
+                    <img class="my-3 img-fluid" src="images/creative-team.svg" alt="creative-team" />
+                </div>
+                <div class="col-lg-6 col-md-7 px-2" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="h3 mb-2">بهترین رزومه ساز کدام است؟</div>
+                    <p class="text-secondary mb-2">رزومه ساز سما، حرفه ای ترین رزومه ساز آنلاین ایرانی است که در آن می‌توانید رزومه کاری و رزومه تحصیلی خود را در کمتر از 10 دقیقه به زبان فارسی بسازید و فایل PDF رزومه تان را به طور نامحدود دانلود کنید.</p>
+                    <p class="text-secondary mb-2">استفاده از رزومه ساز، فرآیند ایجاد رزومه را به طور قابل توجهی سریعتر و آسان‌تر می کند. 
+                        با یک نرم افزار ساخت رزومه، شما بدون نگرانی نسبت به مشکلات ایجاد رزومه مانند انتخاب فونت، طرح‌بندی، قالب‌بندی و غیره به محتوای رزومه فکر می‌کنید. تنها کاری که باید انجام دهید این است که یک قالب رزومه انتخاب کرده، فرم اطلاعات رزومه را پر کنید و فایل رزومه شما آماده دانلود است!</p>
+                </div>
+            </div>
+            <div class="row py-3 py-lg-5">
+                <div class="col-lg-6 col-md-5 px-3 order-md-last">
+                    <img class="my-3 img-fluid" src="images/work-chat.svg" alt="work-chat" />
+                </div>
+                <div class="col-lg-6 col-md-7 px-2">
+                    <div class="h3 mb-2">چرا برای ساخت رزومه بهتر است از رزومه ساز آنلاین استفاده کنیم؟</div>
+                    <p class="text-secondary mb-3">
+                        همه ما می‌دانیم انجام هر کاری با ابزار مناسب، خیلی راحت و لذت‌بخش خواهد بود.
+                        ابزارهای مختلفی برای ساخت رزومه وجود دارند که یکی از بهترین آنها، رزومه ساز آنلاین سما می‌باشد.
+                        <br />
+                        دلایلی که اثبات می کند بهترین راه برای رزومه سازی در اختیار دارید:
+                    </p>
+                    <ul class="nav nav-tabs nav-fill" role="tablist">
+                        <li class="nav-item" role="presentation"><a class="nav-link link-warning p-1 active" aria-current="page" href="#experience" data-bs-toggle="tab"><span class="badge bg-secondary ms-2">1</span>هوشمند</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link link-warning p-1" href="#flexibility" data-bs-toggle="tab"><span class="badge bg-secondary me-1 ms-2">2</span>صرفه جویی در زمان </a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link link-warning p-1" href="#results" data-bs-toggle="tab"><span class="badge bg-secondary me-1 ms-2">3</span>نمونه رزومه آماده</a></li>
+                    </ul>
+                    <div class="tab-content border border-top-0 text-secondary p-3" id="myTabContent">
+                        <div class="tab-pane fade show active" id="experience" role="tabpanel" aria-labelledby="home-tab">
+                            <p>نگران عنوان و جایگاه بخش‌های رزومه نباشید!</p>
+                            <p>تمام کاری که شما انجام می‌دهید وارد کردن اطلاعات است و رزومه ساز آنلاین اطلاعات شما را دریافت کرده و در قالب و فرمت استاندارد نمایش می‌دهد</p>
+                        </div>
+                        <div class="tab-pane fade" id="flexibility" role="tabpanel" aria-labelledby="profile-tab">
+                            <p> رزومه ساز سما می‌تواند به شما کمک کند تا زمان بیشتری برای فعالیت‌های دیگر داشته باشید و همچنین رزومه‌ی خود را بهبود بخشید.</p>
+                            <p>با استفاده از رزومه ساز آنلاین و اطلاعات از قبل آماده شده، می‌توانید در مدت 10 دقیقه رزومه خود را بسازید.</p>
+                        </div>
+                        <div class="tab-pane fade" id="results" role="tabpanel" aria-labelledby="contact-tab">
+                            <p>راهنمای کامل برای ساخت رزومه است.</p>
+                            <p>در صورت نداشتن اطلاعات کافی درباره رزومه، با مطالعه چندین نمونه رزومه به راحتی می‌توانید رزومه خود را با محتوای صحیح و استاندارد بسازید.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!--btn-top-->
+    <a href="#" class="btn-top">
+        <img src="images/arrow-up.png" alt="arrow" style=" width:40px; height:40px;" />
+    </a>
+    <!--btn-top-->
+<?php require_once 'includes/footer.php'; ?>
