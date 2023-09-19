a12594c97137d60b86eecb335d202eb7031c28ab
Samane <133226049+samanezarini@users.noreply.github.com>
2023-09-19 15:57:02 +0330

    property
    property

---
+++ property.php
@@ -0,0 +1,116 @@
+﻿<?php
+require_once 'includes/dbconfig.php';
+$title = 'رزومه ساز سما';
+require_once 'includes/header.php';
+?>
+    <!--Body content-->
+    <div class="section pt-3 pt-lg-5 px-2 px-lg-4">
+        <div class="container-narrow">
+            <div class="text-center mb-4">
+                <h2>ویژگی های رزومه ساز سما</h2>
+            </div>
+            <div class="text-center">
+                <p class="mx-auto mb-3" style="max-width: 800px">سرویس مدیریت رزومه‌ی ما با ویژگی‌های متمایزکننده فرآیند رزومه‌سازی را آسان‌تر کرده است.</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row py-3">
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-4 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/palete.png" alt="design" style="width:60px; height:60px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">طراحی آسان</h3>
+                        <p class="text-small text-secondary">رزومه ساز سما ابزاری ساده و کاربرپسند دارد که به شما کمک می کند رزومه خود را به راحتی طراحی کنید.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/copy.png" alt="format" style="width:60px; height:60px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">قالب های متنوع</h3>
+                        <p class="text-small text-secondary"> رزومه ساز سما قالب های متنوعی را برای شما فراهم می کند که می توانید برای طراحی رزومه خود از آنها استفاده کنید.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/save.png" alt="save" style="width:55px; height:55px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">ذخیره سازی آنلاین</h3>
+                        <p class="text-small text-secondary">با استفاده از رزومه ساز سما، می توانید رزومه خود را آنلاین ذخیره کنید و همیشه به آن دسترسی داشته باشید.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/pdf.png" alt="palet" style="width:60px; height:60px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">فرمت‌ فایل رزومه</h3>
+                        <p class="text-small text-secondary">پس از ساخت رزومه می‌توانید فایل رزومه خود را با فرمت‌ PDF دریافت کرده و برای فرصت‌های شغلی و تحصیلی استفاده کنید.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/add-file.png" alt="palet" style="width:60px; height:60px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">ایجاد چندین رزومه</h3>
+                        <p class="text-small text-secondary">شما می توانید بیش از یک رزومه با تنظیمات مختلف ایجاد کنید.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/cardit.png" alt="palet" style="width:65px; height:65px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">بدون پرداخت هزینه</h3>
+                        <p class="text-small text-secondary">رزومه ساز انلاین سما رایگان است و شما می توانید بدون هیچ هزینه ای رزومه خود را بسازید.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/cyber.png" alt="palet" style="width:60px; height:60px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">امنیت بالا</h3>
+                        <p class="text-small text-secondary">رزومه ساز از نظر امنیتی بسیار قوی است و اطلاعات شما به صورت محرمانه و امن ذخیره می کند.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="p-3 bg-light mb-3 size-box">
+                        <div class="text-center text-secondary display-3">
+                            <img src="images/capability.png" alt="palet" style="width:70px; height:65px; opacity:0.7;" />
+                        </div>
+                        <h3 class="h4 mt-2">سازگاری رزومه</h3>
+                        <p class="text-small text-secondary">رزومه ساز سما به شما این امکان را می دهد که رزومه خود را بر روی دستگاه های مختلف خود مشاهده کنید.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <formats>
+            <div class="row col-lg-12 col-md-12 px-4 py-4 border-0 bg-light rounded">
+                <div class="h2 text-center">قالب های رزومه ساز سما</div>
+                <p class="text-center">با استفاده از بخش های از پیش تعریف شده و تایید شده توسط کارفرمایان، ساختن رزومه خود را شروع کرده سپس آن را بر اساس نیازتان سفارشی کنید. استفاده از رزومه سما بسیار آسان است، حتی اگر قبلاً هرگز رزومه ای تهیه نکرده باشید!</p>
+                <div class="col f1 text-center me-3 mb-4 mt-3 px-2 py-2">
+                    <a href="template1.html"><img src="images/format/1280.jpg" alt="format1" class="img-fluid pic1" style="width:100%; height:100%; border-radius:20px;"></a>
+                </div>
+                <div class="col f2 text-center me-3 mb-4 mt-3 px-2 py-2">
+                    <a href="template2.html"><img src="images/format/1281.jpg" alt="format1" class="img-fluid pic2" style="width:100%; height:100%; border-radius:20px;"></a>
+                </div>
+                <div class="col f3 text-center me-3 mb-4 mt-3 px-2 py-2">
+                    <a href="temp1.html"><img src="images/format/1282.jpg" alt="format1" class="img-fluid pic3" style="width:100%; height:100%; border-radius:20px;"></a>
+                </div>
+                <div class="col f4 text-center me-3 mb-4 mt-3 px-2 py-2">
+                    <a href="#">
+                        <img src="images/format/1283.jpg" alt="format1" class="img-fluid pic4" style="width:100%; height:100%; border-radius:20px;">
+                    </a>
+                </div>
+            </div>
+        </formats>
+
+<?php require_once 'includes/footer.php'; ?>
