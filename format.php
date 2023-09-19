dc16611455bd3c4739cba79caebd7bfb5a1f8ad0
Samane <133226049+samanezarini@users.noreply.github.com>
2023-09-19 15:50:33 +0330

    format
    format

---
+++ format.php
@@ -0,0 +1,71 @@
+﻿<?php
+require_once 'includes/dbconfig.php';
+$title = 'قالب ها';
+require_once 'includes/header.php';
+?>
+
+
+    <div class="container my-5">
+        <div class="site-items">
+            <h5 class="text-center mb-3">نمونه رزومه های کاری ساخته شده توسط رزومه ساز سما</h5>
+            <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+                <strong>قالب رزومه</strong> مد نظر خود را انتخاب کنید و در کمترین زمان آن را بسازید.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <div class="row py-3">
+                <div class="col-lg-4 col-md-6 my-2 px-4 py-4">
+                    <div class="site-item">
+                        <div class="shadow-sm">
+                            <a href="temp1.html">
+                                <img class="img-fluid template" src="images/format/1281.jpg" width="800" height="600" alt="Material Landing">
+                            </a>
+                        </div>
+                        <div class="text-center my-3 px-2">
+                            <h5 class="mt-4 mb-2">
+                                <a class="link-alt" href="/templates/material-landing/">رزومه طراح و گرافیست</a>
+                            </h5>
+                            <div class="text-muted text-small">
+                                رزومه دوستونه با طراحی آژور
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 my-2 px-4 py-4">
+                    <div class="site-item">
+                        <div class="shadow-sm">
+                            <a href="template1.html">
+                                <img class="img-fluid template" src="images/format/1280.jpg" width="800" height="600" alt="Material Landing">
+                            </a>
+                        </div>
+                        <div class="text-center my-3 px-2">
+                            <h5 class="mt-4 mb-2">
+                                <a class="link-alt" href="/templates/material-landing/">رزومه کارمندی و اداری</a>
+                            </h5>
+                            <div class="text-muted text-small">
+                                رزومه حرفه ای با طراحی ساده و روان
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 my-2 px-4 py-4">
+                    <div class="site-item">
+                        <div class="shadow-sm">
+                            <a href="template2.html">
+                                <img class="img-fluid template" src="images/format/1287.jpg" width="800" height="600" alt="Material Landing">
+                            </a>
+                        </div>
+                        <div class="text-center my-3 px-2">
+                            <h5 class="mt-4 mb-2">
+                                <a class="link-alt" href="template2.html">رزومه مهندس کامپیوتر</a>
+                            </h5>
+                            <div class="text-muted text-small">
+                                رزومه مرتب با طراحی مدرن و جدید
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+<?php require_once 'includes/footer.php' ?>
